142763966f55b534cbc01d14aadbc363a4f8755c
Oliver BINNS <mail@oliverbinns.co.uk>
2023-01-20 23:32:37 +0000

    Refactored to extract concurrency implementation to extension

--- Conferences/Persistence/CloudKit/PublicDatabase.swift
+++ Conferences/Persistence/CloudKit/PublicDatabase.swift
@@ -12,49 +12,18 @@ struct PublicDatabase {
         let predicate = NSPredicate(value: true)
         let query = CKQuery(recordType: RecordType.conference.rawValue, predicate: predicate)
         
-        let operation = CKQueryOperation(query: query)
-        operation.qualityOfService = .userInteractive
-        
-        return try await withThrowingTaskGroup(of: T?.self) { group in
-            group.addTask {
-                try await withCheckedThrowingContinuation { continuation in
-                    operation.recordMatchedBlock = { recordID, result in
-                        switch result {
-                        case .success(let record):
-                            Task {
-                                let value = try await T(record: record, database: self)
-                                continuation.resume(with: .success(value))
-                            }
-                        case .failure(let error):
-                            continuation.resume(with: .failure(error))
-                        }
-                    }
-                }
-            }
-            
-            container.publicCloudDatabase.add(operation)
-            
-            return try await group
-                .compactMap { $0 }
-                .reduce(into: [T](), {
-                    $0.append($1)
-                })
-        }
+        return try await container.publicCloudDatabase
+            .run(query: query)
+            .compactMap { try await T(record: $0, database: self) }
+            .reduce(into: [T](), {
+                $0.append($1)
+            })
     }
     
     func retrieve<T: Queryable>(id: CKRecord.ID, ofType type: RecordType) async throws -> T? {
-        try await withCheckedThrowingContinuation { continuation in
-            container.publicCloudDatabase
-                .fetch(withRecordID: id) { record, error in
-                    Task {
-                        if let record {
-                            try await continuation.resume(with: .success(T(record: record, database: self)))
-                        } else if let error {
-                            continuation.resume(with: .failure(error))
-                        }
-                    }
-                }
-        }
+        guard let record = try await container.publicCloudDatabase
+            .fetch(withRecordID: id) else { return nil }
+        return try await T(record: record, database: self)
     }
     
     enum RecordType: String {
@@ -62,3 +31,4 @@ struct PublicDatabase {
         case venue = "Venue"
     }
 }
+
