efa493e7c937933c07fdc0539137619b2cf3eae3
Oliver BINNS <mail@oliverbinns.co.uk>
2023-06-20 23:06:27 +0100

    chore: add extra logging

--- verify-commit-messages.sh
+++ verify-commit-messages.sh
@@ -11,6 +11,8 @@ echo $commits |
 while read -r line; do
   check=$(echo $line | egrep '^(docs|fix|feat|chore|style|refactor|perf|test)(?:\((.*)\))?(!?)\: (.*)$')
 
+  echo "commit ok: $line"
+
   if [ "" = "$check" ]; then
     echo "Commit Message did not conform to Conventional Commits:"
     echo "\"$line\""
