2899edee7a0b065b5d5c5e5edc20e26752a023dd
Oliver BINNS <mail@oliverbinns.co.uk>
2023-06-20 23:05:42 +0100

    chore: simplify script

--- verify-commit-messages.sh
+++ verify-commit-messages.sh
@@ -3,19 +3,17 @@
 FROM="origin/main"
 TO="$(git branch --show-current)"
 
-commits=$(git log --pretty=format:'%h %s' --abbrev-commit --date=relative $FROM..$TO)
+commits=$(git log --pretty=format:'%s' --date=relative $FROM..$TO)
 
 echo $commits
 
 echo $commits |
 while read -r line; do
-  MSG=${line:8}
-
-  check=$(echo $MSG | egrep '^(docs|fix|feat|chore|style|refactor|perf|test)(?:\((.*)\))?(!?)\: (.*)$')
+  check=$(echo $line | egrep '^(docs|fix|feat|chore|style|refactor|perf|test)(?:\((.*)\))?(!?)\: (.*)$')
 
   if [ "" = "$check" ]; then
     echo "Commit Message did not conform to Conventional Commits:"
-    echo "\"$MSG\""
+    echo "\"$line\""
     exit 1
   fi
 done
